45b772f920a15e736f02446d17d6def0cca78b7f
vegeta <hoangvmh@gmail.com>
2019-01-12 09:22:04 +0900

    Add Splah screen and Register screen

---
+++ app/src/main/java/giavu/hoangvm/hh/activity/register/RegisterAccountActivity.kt
@@ -0,0 +1,14 @@
+package giavu.hoangvm.hh.activity.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import giavu.hoangvm.hh.R
+
+class RegisterAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_account)
+
+    }
+}
