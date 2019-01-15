38b98809c781b902f3a4d0ad5718494b3949812b
vegeta <hoangvmh@gmail.com>
2019-01-15 23:28:49 +0900

    Add splash screen

--- app/src/main/java/giavu/hoangvm/hh/activity/splash/SplashActivity.kt
+++ app/src/main/java/giavu/hoangvm/hh/activity/splash/SplashActivity.kt
@@ -3,11 +3,13 @@ package giavu.hoangvm.hh.activity.splash
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import giavu.hoangvm.hh.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        animation_view.playAnimation()
     }
 }
