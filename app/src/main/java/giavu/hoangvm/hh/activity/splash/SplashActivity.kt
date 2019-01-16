fd4c7330c38a00654edb30dbcf2e6243c4209dfc
vegeta <hoangvmh@gmail.com>
2019-01-17 00:59:28 +0900

    Remove title bar

--- app/src/main/java/giavu/hoangvm/hh/activity/splash/SplashActivity.kt
+++ app/src/main/java/giavu/hoangvm/hh/activity/splash/SplashActivity.kt
@@ -1,6 +1,7 @@
 package giavu.hoangvm.hh.activity.splash
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import giavu.hoangvm.hh.R
 import kotlinx.android.synthetic.main.activity_splash.*
@@ -10,7 +11,8 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        actionBar?.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        //actionBar?.hide()
         animation_view.playAnimation()
     }
 }
