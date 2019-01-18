4e21827ce20ac2bcc0da407efca6c08c35404914
vegeta <hoangvmh@gmail.com>
2019-01-19 08:11:18 +0900

    Add validation class

---
+++ app/src/main/java/giavu/hoangvm/hh/validation/ValidationPattern.kt
@@ -0,0 +1,16 @@
+package giavu.hoangvm.hh.validation
+
+import java.util.regex.Pattern
+
+/**
+ * @Author: Hoang Vu
+ * @Date:   2019/01/19
+ */
+object ValidationPattern {
+    val PHONE_NUMBER = Pattern.compile("\\d{7,15}")
+    val EMAIL_ADDRESS = Pattern.compile(
+            "\\A\\s*[^@\\s]{1,64}" + "\\@" + "((?:[-\\p{L}\\d]+\\.)+\\p{L}{2,})\\s*\\z"
+    )
+    val KANA_NAME = Pattern.compile("\\A[ァ-ー\\s]{1,20}\\z")
+    val PASSWORD = Pattern.compile("\\w{6,20}$")
+}
