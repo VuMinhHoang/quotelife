24e4dafbd86e434558a0c6647f901e605d6ca355
bejiita <hoangvmh@gmail.com>
2019-01-21 16:07:56 +0900

    Test Badge

--- app/src/main/java/giavu/hoangvm/hh/hoi/HOIApp.kt
+++ app/src/main/java/giavu/hoangvm/hh/hoi/HOIApp.kt
@@ -1,9 +1,16 @@
 package giavu.hoangvm.hh.hoi
 
+import android.app.Activity
 import android.app.Application
+import android.content.Context
+import android.os.Bundle
 import android.util.Log
 import android.webkit.WebView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import com.facebook.stetho.Stetho
+import com.github.arturogutierrez.Badges
+import com.github.arturogutierrez.BadgesNotSupportedException
 import com.google.firebase.FirebaseApp
 import giavu.hoangvm.hh.BuildConfig
 import giavu.hoangvm.hh.firebase.FcmToken
@@ -11,8 +18,11 @@ import giavu.hoangvm.hh.firebase.FcmTokenStore
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
+import me.leolin.shortcutbadger.ShortcutBadger
 import org.koin.android.ext.android.inject
 import timber.log.Timber
+
+
 
 /**
  * @Author: Hoang Vu
@@ -20,6 +30,7 @@ import timber.log.Timber
  */
 open class HOIApp : Application() {
 
+    private val TAG = HOIApp::class.java.simpleName
     private val fcmTokenStore: FcmTokenStore by inject()
 
     override fun onCreate() {
@@ -33,6 +44,7 @@ open class HOIApp : Application() {
 
             WebView.setWebContentsDebuggingEnabled(true)
         }
+        registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
         KoinInitializer(this).initialize()
         FirebaseApp.initializeApp(this)
         initialize()
@@ -40,6 +52,7 @@ open class HOIApp : Application() {
 
     private fun initialize() {
         initFcm()
+        initBadge()
     }
 
     private fun initFcm() {
@@ -53,9 +66,44 @@ open class HOIApp : Application() {
                 )
     }
 
+    private fun initBadge() {
+        Log.d(TAG, ShortcutBadger.isBadgeCounterSupported(this).toString())
+        ShortcutBadger.applyCount(this, 17)
+    }
+
     private fun observeFcmToken() {
         fcmTokenStore.token.observeForever { token ->
             Log.d("Token", token)
+        }
+    }
+
+    private val activityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks{
+        override fun onActivityPaused(activity: Activity?) {
+            Log.d(TAG, "onActivityPaused")
+        }
+
+        override fun onActivityResumed(activity: Activity?) {
+            Log.d(TAG, "onActivityResumed")
+        }
+
+        override fun onActivityStarted(activity: Activity?) {
+            Log.d(TAG, "onActivityStarted")
+        }
+
+        override fun onActivityDestroyed(activity: Activity?) {
+            Log.d(TAG, "onActivityDestroyed")
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+            Log.d(TAG, "onActivitySaveInstanceState")
+        }
+
+        override fun onActivityStopped(activity: Activity?) {
+            Log.d(TAG, "onActivityStopped")
+        }
+
+        override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+            Log.d(TAG, "onActivityCreated")
         }
     }
 }
