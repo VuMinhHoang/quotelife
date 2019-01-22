69f426c4ee767baff458f399a9903caee52478bc
bejiita <hoangvmh@gmail.com>
2019-01-23 08:17:27 +0900

    Add user token header

--- app/src/main/java/giavu/hoangvm/hh/core/retrofit/JFDApiAccessor.kt
+++ app/src/main/java/giavu/hoangvm/hh/core/retrofit/JFDApiAccessor.kt
@@ -3,6 +3,7 @@ package giavu.hoangvm.hh.core.retrofit
 import android.content.Context
 import giavu.hoangvm.hh.R
 import giavu.hoangvm.hh.core.graphql.ApiHeader
+import giavu.hoangvm.hh.helper.UserSharePreference
 
 /**
  * @Author: Hoang Vu
@@ -16,6 +17,7 @@ class JFDApiAccessor(private val context: Context) : ApiAccessor(context = conte
     override fun onCreateHeaders(headers: MutableMap<String, String>) {
         headers.put(ApiHeader.KEY_AUTHORIZATION, context.getString(R.string.quote_api_key))
         headers.put(ApiHeader.CONTENT_TYPE, ApiHeader.VALUE_ACCEPT_JSON)
+        headers.put(ApiHeader.KEY_USER_AGENT, UserSharePreference.fromContext(context).getUserSession())
     }
 
     fun from(): ApiAccessor {
