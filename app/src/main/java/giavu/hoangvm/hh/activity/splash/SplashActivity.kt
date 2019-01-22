7fa5a5df6b98351ea70bccbd03fd71f4897084f1
vegeta <hoangvmh@gmail.com>
2019-01-22 22:14:50 +0900

    Delete testing method

--- app/src/main/java/giavu/hoangvm/hh/activity/splash/SplashActivity.kt
+++ app/src/main/java/giavu/hoangvm/hh/activity/splash/SplashActivity.kt
@@ -1,7 +1,6 @@
 package giavu.hoangvm.hh.activity.splash
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import giavu.hoangvm.hh.R
@@ -11,13 +10,8 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
-import okhttp3.*
 import org.koin.android.ext.android.inject
 import timber.log.Timber
-import java.io.IOException
-import java.net.URI
-import java.net.URISyntaxException
-import java.net.URL
 import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
@@ -37,7 +31,6 @@ class SplashActivity : AppCompatActivity() {
     private fun initialize() {
         // Temporary implement
         checkLocalData()
-        testing()
     }
 
     private fun checkLocalData() {
@@ -59,29 +52,4 @@ class SplashActivity : AppCompatActivity() {
         this@SplashActivity.finish()
     }
 
-    private fun testing() {
-        val request = Request.Builder()
-        try {
-            val httpUrl = HttpUrl.parse("http://pic1.nipic.com|/2008-12-30/200812308231244_2.jpg")?.uri()
-            httpUrl?.let {
-                request.url(it.toURL())
-            }
-        }catch (e: Exception) {
-            Log.d("Testing", e.toString())
-            return
-        }
-
-
-        val client = OkHttpClient.Builder().build()
-
-        client.newCall(request.build()).enqueue(object: Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.d("Testing", e.toString())
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                Log.d("Testing", response.body().toString())
-            }
-        })
-    }
 }
