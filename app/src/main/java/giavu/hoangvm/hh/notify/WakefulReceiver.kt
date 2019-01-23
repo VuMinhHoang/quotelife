6dd9c65887241605226581e4d70b3c350f77029a
bejiita <hoangvmh@gmail.com>
2019-01-23 20:27:08 +0900

    hanlder receiver

--- app/src/main/java/giavu/hoangvm/hh/notify/WakefulReceiver.kt
+++ app/src/main/java/giavu/hoangvm/hh/notify/WakefulReceiver.kt
@@ -6,6 +6,11 @@ import android.content.Intent
 
 class WakefulReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.action?.let {action ->
+            if (action.equals(Intent.ACTION_BOOT_COMPLETED)){
+                
+            }
+        }
 
     }
 }
