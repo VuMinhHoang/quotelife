da06e9b369aec7f2b4e034c46d2d239e27ff9926
bejiita <hoangvmh@gmail.com>
2019-01-23 20:28:43 +0900

    Add testing file

--- app/src/main/java/giavu/hoangvm/hh/notify/WakefulReceiver.kt
+++ app/src/main/java/giavu/hoangvm/hh/notify/WakefulReceiver.kt
@@ -3,12 +3,13 @@ package giavu.hoangvm.hh.notify
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import timber.log.Timber
 
 class WakefulReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         intent?.action?.let {action ->
             if (action.equals(Intent.ACTION_BOOT_COMPLETED)){
-                
+                Timber.d("Boot completed !")
             }
         }
 
