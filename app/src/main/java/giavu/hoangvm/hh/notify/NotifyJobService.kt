1e297007e629f829ce5fcddc8b4b9491a8369337
bejiita <hoangvmh@gmail.com>
2019-01-23 20:22:23 +0900

    Add notify service

---
+++ app/src/main/java/giavu/hoangvm/hh/notify/NotifyJobService.kt
@@ -0,0 +1,14 @@
+package giavu.hoangvm.hh.notify
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+class NotifyJobService: JobService() {
+    override fun onStopJob(params: JobParameters?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/giavu/hoangvm/hh/notify/WakefulReceiver.kt
@@ -0,0 +1,11 @@
+package giavu.hoangvm.hh.notify
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class WakefulReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
