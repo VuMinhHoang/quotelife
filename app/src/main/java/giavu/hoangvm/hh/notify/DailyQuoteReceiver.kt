64222a7a0ff01b3e0b57ac19cc04a0a278461c02
bejiita <hoangvmh@gmail.com>
2019-01-24 13:59:34 +0900

    Add DailyQuoteReceiver

--- app/src/main/java/giavu/hoangvm/hh/notify/DailyQuoteReceiver.kt
+++ app/src/main/java/giavu/hoangvm/hh/notify/DailyQuoteReceiver.kt
@@ -3,15 +3,17 @@ package giavu.hoangvm.hh.notify
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import timber.log.Timber
 
-class WakefulReceiver: BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        intent?.action?.let {action ->
+class DailyQuoteReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+
+        intent.action?.let {action ->
             if (action.equals(Intent.ACTION_BOOT_COMPLETED)){
-                Timber.d("Boot completed !")
+                val service = Intent(context, NotificationDailyQuoteService::class.java).apply {
+                    putExtra("Hello", intent.getStringExtra("Hello"))
+                }
+                context.startService(service)
             }
         }
-
     }
 }
