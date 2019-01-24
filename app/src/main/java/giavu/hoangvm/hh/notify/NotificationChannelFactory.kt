ce109c615a76ce8d2b2c3985ceb26486fa7bff98
bejiita <hoangvmh@gmail.com>
2019-01-24 13:59:57 +0900

    Add factory chanel

--- app/src/main/java/giavu/hoangvm/hh/notify/NotificationChannelFactory.kt
+++ app/src/main/java/giavu/hoangvm/hh/notify/NotificationChannelFactory.kt
@@ -5,7 +5,6 @@ import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
-import jp.co.nikko_data.japantaxi.R
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 class NotificationChannelFactory(private val context: Context) {
@@ -13,16 +12,9 @@ class NotificationChannelFactory(private val context: Context) {
     fun create(channel: NotificationChannels): NotificationChannel = when (channel) {
         NotificationChannels.SERVICE -> {
             NotificationChannel(
-                    context.getString(R.string.notification_channel_id_service),
-                    context.getString(R.string.notification_channel_name_service),
+                    "CHANEL_ID_SERVICE",
+                    "CHANEL_ID_SERVICE_NAME",
                     NotificationManager.IMPORTANCE_DEFAULT
-            )
-        }
-        NotificationChannels.DISPATCH -> {
-            NotificationChannel(
-                    context.getString(R.string.notification_channel_id_dispatch),
-                    context.getString(R.string.notification_channel_name_dispatch),
-                    NotificationManager.IMPORTANCE_HIGH
             )
         }
     }
