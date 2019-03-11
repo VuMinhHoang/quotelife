96bc0334f73ef606e739b8050e57b1afcc4ddf87
vuvegeta <hoangvmh@gmail.com>
2019-03-11 16:52:31 +0900

    init state network

--- app/src/main/java/giavu/hoangvm/hh/activity/quotelist/QuoteListActivity.kt
+++ app/src/main/java/giavu/hoangvm/hh/activity/quotelist/QuoteListActivity.kt
@@ -3,6 +3,7 @@ package giavu.hoangvm.hh.activity.quotelist
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -28,6 +29,7 @@ class QuoteListActivity : AppCompatActivity() {
         viewModel = ViewModelProviders.of(this)
             .get(QuoteListViewModel::class.java)
         initAdapter()
+        initState()
     }
 
     private fun initAdapter() {
@@ -39,5 +41,16 @@ class QuoteListActivity : AppCompatActivity() {
                 quoteListAdapter.submitList(it)
             }
         )
+    }
+
+    private fun initState() {
+        txt_error.setOnClickListener { viewModel.retry() }
+        viewModel.getState().observe(this, Observer { state ->
+            progress_bar.visibility = if (viewModel.listIsEmpty() && state == State.LOADING) View.VISIBLE else View.GONE
+            txt_error.visibility = if (viewModel.listIsEmpty() && state == State.ERROR) View.VISIBLE else View.GONE
+            if (!viewModel.listIsEmpty()) {
+                quoteListAdapter.setState(state ?: State.DONE)
+            }
+        })
     }
 }
