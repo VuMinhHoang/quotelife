f653871b94958c5800129603eb3e8b5416d972a7
vuvegeta <hoangvmh@gmail.com>
2019-03-11 16:34:18 +0900

    Add Adapter for Quote

---
+++ app/src/main/java/giavu/hoangvm/hh/activity/quotelist/QuoteViewHolder.kt
@@ -0,0 +1,30 @@
+package giavu.hoangvm.hh.activity.quotelist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import giavu.hoangvm.hh.R
+import giavu.hoangvm.hh.model.Quote
+import kotlinx.android.synthetic.main.item_quote.view.*
+
+/**
+ * @Author: Hoang Vu
+ * @Date:   2019/03/11
+ */
+class QuoteViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    fun bind(quote: Quote?) {
+        if(quote != null) {
+            itemView.quote_body.text = quote.body
+        }
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): QuoteViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_quote, parent, false)
+            return QuoteViewHolder(view)
+        }
+    }
+}
