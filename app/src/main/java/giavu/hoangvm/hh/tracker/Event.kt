0322d1967b2ebd5f750207b933d3668c0af190b5
vegeta20 <hoangvmh@gmail.com>
2019-03-19 23:16:38 +0900

    implement firebase

--- app/src/main/java/giavu/hoangvm/hh/tracker/Event.kt
+++ app/src/main/java/giavu/hoangvm/hh/tracker/Event.kt
@@ -4,4 +4,5 @@ sealed class Event(val eventName: String) {
     object FirstOpen : Event("HOI_first_open")
     object LoginSuccess: Event("login_success")
     object LoginFailure: Event("login_failure")
+    object IMPSRegister: Event("imps_register")
 }
