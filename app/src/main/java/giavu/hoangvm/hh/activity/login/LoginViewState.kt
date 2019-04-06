a61ef0aadd8a0c71d8cea675439dd2e88f0293f6
vegeta20 <hoangvmh@gmail.com>
2019-04-06 18:57:12 +0900

    Add loginViewState

---
+++ app/src/main/java/giavu/hoangvm/hh/activity/login/LoginViewState.kt
@@ -0,0 +1,24 @@
+package giavu.hoangvm.hh.activity.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+
+/**
+ * @Author: Hoang Vu
+ * @Date:   2019/04/06
+ */
+class LoginViewState(
+    private val _userName : LiveData<String>,
+    private val _password : LiveData<String>
+) {
+
+    val loginBtnEnable : LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+
+        val observer : Observer<String> = Observer {
+            this.value = (_userName.value.orEmpty() + _password.value.orEmpty()).isNotEmpty()
+        }
+        addSource(_userName, observer)
+        addSource(_password, observer)
+    }
+}
