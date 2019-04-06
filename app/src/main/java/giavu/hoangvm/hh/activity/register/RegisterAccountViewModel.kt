3913cab82c22d325cb06d5b873463fb132845852
vegeta20 <hoangvmh@gmail.com>
2019-04-06 12:39:58 +0900

    Refactoring

--- app/src/main/java/giavu/hoangvm/hh/activity/register/RegisterAccountViewModel.kt
+++ app/src/main/java/giavu/hoangvm/hh/activity/register/RegisterAccountViewModel.kt
@@ -1,23 +1,19 @@
 package giavu.hoangvm.hh.activity.register
 
 import android.app.Application
-import androidx.lifecycle.*
-import giavu.hoangvm.hh.R
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
 import giavu.hoangvm.hh.api.UserApi
-import giavu.hoangvm.hh.extension.combineTripleLatest
 import giavu.hoangvm.hh.helper.ResourceProvider
 import giavu.hoangvm.hh.model.RegBody
 import giavu.hoangvm.hh.model.RegUser
-import giavu.hoangvm.hh.validation.EmailAddressValidator
-import giavu.hoangvm.hh.validation.PasswordValidator
-import giavu.hoangvm.hh.validation.UserNameValidator
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
-import timber.log.Timber
 
 /**
  * @Author: Hoang Vu
@@ -29,66 +25,13 @@ class RegisterAccountViewModel(private val resourceProvider: ResourceProvider, a
     private lateinit var navigator: RegisterAccountNavigator
     private val userApi: UserApi by application.inject()
 
-    private val passwordValidator = PasswordValidator()
-    private val emailValidator = EmailAddressValidator()
-    private val userNameValidator = UserNameValidator()
-
-    private val compositeDisposable by lazy {
-        CompositeDisposable()
-    }
-    private val _userName = MutableLiveData<String>()
-    private val _userNameError = MutableLiveData<String>()
-    private val _email = MutableLiveData<String>()
-    private val _emailError = MutableLiveData<String>()
-    private val _password = MutableLiveData<String>()
-    private val _passwordError = MutableLiveData<String>()
-    private val _registerButtonEnabled = MutableLiveData<Boolean>()
-
-    val registerButtonEnabled: LiveData<Boolean>
-        get() = _registerButtonEnabled
-
-    val userNameError: LiveData<String>
-        get() = _userNameError
-    val emailError: LiveData<String>
-        get() = _emailError
-    val passwordError: LiveData<String>
-        get() = _passwordError
+    private val compositeDisposable by lazy { CompositeDisposable() }
+    val userName = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
 
     fun initialize(navigator: RegisterAccountNavigator, owner: LifecycleOwner) {
         this.navigator = navigator
-        _registerButtonEnabled.value = false
-        _userName.value = ""
-        _password.value = ""
-        _email.value = ""
-        _userNameError.value = null
-        checkValidInput(owner)
-    }
-
-    fun onUserNameInput(text: CharSequence) {
-        _userName.value = text.toString()
-        if (text.isNotEmpty() && !userNameValidator.verify(text)) {
-            _userNameError.value = resourceProvider.getString(R.string.error_username)
-        } else {
-            _userNameError.value = null
-        }
-    }
-
-    fun onEmailInput(text: CharSequence) {
-        _email.value = text.toString()
-        if (text.isNotEmpty() && !emailValidator.verify(text)) {
-            _emailError.value = resourceProvider.getString(R.string.error_email)
-        } else {
-            _emailError.value = null
-        }
-    }
-
-    fun onPasswordInput(text: CharSequence) {
-        _password.value = text.toString()
-        if (text.isNotEmpty() && !passwordValidator.verify(text)) {
-            _passwordError.value = resourceProvider.getString(R.string.error_password)
-        } else {
-            _passwordError.value = null
-        }
     }
 
     fun gotoLogin() {
@@ -97,9 +40,9 @@ class RegisterAccountViewModel(private val resourceProvider: ResourceProvider, a
 
     fun register() {
         val body = RegBody(
-            login = _userName.value,
-            email = _email.value,
-            password = _password.value
+            login = userName.value,
+            email = email.value,
+            password = password.value
         )
         val user = RegUser(
             user = body
@@ -119,27 +62,4 @@ class RegisterAccountViewModel(private val resourceProvider: ResourceProvider, a
             )
             .addTo(compositeDisposable)
     }
-
-    private fun checkValidInput(owner: LifecycleOwner) {
-        combineTripleLatest(
-            source1 = _userName.toPublisher(owner),
-            source2 = _email.toPublisher(owner),
-            source3 = _password.toPublisher(owner)
-        )
-            .map { triple ->
-                val userName = triple.first
-                val email = triple.second
-                val password = triple.third
-                userNameValidator.verify(userName)
-                        && emailValidator.verify(email)
-                        && passwordValidator.verify(password)
-
-            }
-            .subscribeBy(
-                onError = Timber::w,
-                onNext = { _registerButtonEnabled.postValue(it) }
-            )
-            .addTo(compositeDisposable)
-    }
-
 }
--- app/src/main/java/giavu/hoangvm/hh/activity/register/RegisterAccountViewState.kt
+++ app/src/main/java/giavu/hoangvm/hh/activity/register/RegisterAccountViewState.kt
@@ -1,8 +1,89 @@
 package giavu.hoangvm.hh.activity.register
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
+import giavu.hoangvm.hh.validation.ValidationPattern
+import java.util.regex.Pattern
+
 /**
  * @Author: Hoang Vu
  * @Date:   2019/04/06
  */
-class RegisterAccountViewState {
+class RegisterAccountViewState(
+    private val _userName: LiveData<String>,
+    private val _email: LiveData<String>,
+    private val _password: LiveData<String>
+) {
+    private val EMPTY = ""
+    private val userNameValidator = RegisterFieldValidator(ValidationPattern.USERNAME)
+    private val emailValidator = RegisterFieldValidator(ValidationPattern.EMAIL_ADDRESS)
+    private val passwordValidator = RegisterFieldValidator(ValidationPattern.PASSWORD)
+
+    private val isUserValid: LiveData<ValidationStatus> = Transformations.map(_userName) {
+        userNameValidator.verify(it)
+    }
+
+    private val isEmailValid: LiveData<ValidationStatus> = Transformations.map(_email) {
+        emailValidator.verify(it)
+    }
+
+    private val isPasswordValid: LiveData<ValidationStatus> = Transformations.map(_password) {
+        passwordValidator.verify(it)
+    }
+
+    val userNameError: LiveData<String> = Transformations.map(isUserValid) {
+        when (it) {
+            ValidationStatus.Valid -> EMPTY
+            ValidationStatus.InValid,
+            ValidationStatus.Ignore -> "Must be a user name"
+        }
+    }
+
+    val emailError: LiveData<String> = Transformations.map(isEmailValid) {
+        when (it) {
+            ValidationStatus.Valid -> EMPTY
+            ValidationStatus.InValid,
+            ValidationStatus.Ignore -> "Must be an email address"
+        }
+    }
+
+    val passwordError: LiveData<String> = Transformations.map(isPasswordValid) {
+        when (it) {
+            ValidationStatus.Valid -> EMPTY
+            ValidationStatus.InValid,
+            ValidationStatus.Ignore -> "Length must 6 - 20"
+        }
+    }
+
+    val registerBtnEnabled: LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+
+        val observer: Observer<ValidationStatus> = Observer {
+            this.value = isEmailValid.value is ValidationStatus.Valid
+                    && isUserValid.value is ValidationStatus.Valid
+                    && isPasswordValid.value is ValidationStatus.Valid
+        }
+        addSource(isUserValid, observer)
+        addSource(isEmailValid, observer)
+        addSource(isPasswordValid, observer)
+    }
+
+    private class RegisterFieldValidator(val pattern: Pattern) {
+        fun verify(charSequence: CharSequence?): ValidationStatus {
+            if (charSequence.isNullOrEmpty()) return ValidationStatus.Ignore
+            return when (pattern.matcher(charSequence).matches()) {
+                true -> ValidationStatus.Valid
+                false -> ValidationStatus.InValid
+            }
+        }
+    }
+
+    private sealed class ValidationStatus {
+        object Valid : ValidationStatus()
+        object InValid : ValidationStatus()
+        object Ignore : ValidationStatus()
+    }
+
+
 }
