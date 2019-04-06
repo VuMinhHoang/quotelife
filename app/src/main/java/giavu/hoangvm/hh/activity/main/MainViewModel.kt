b78fc6be6a269724fd82962c51d547c79d4de3ad
vegeta20 <hoangvmh@gmail.com>
2019-04-07 00:05:11 +0900

    Get MainViewModel from koin

--- app/src/main/java/giavu/hoangvm/hh/activity/main/MainViewModel.kt
+++ app/src/main/java/giavu/hoangvm/hh/activity/main/MainViewModel.kt
@@ -1,24 +1,20 @@
 package giavu.hoangvm.hh.activity.main
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import giavu.hoangvm.hh.api.QuotesApi
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
-import org.koin.android.ext.android.inject
 
 /**
  * @Author: Hoang Vu
  * @Date:   2019/01/05
  */
-class MainViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val quoteApi: QuotesApi by application.inject()
+class MainViewModel(private val quotesApi: QuotesApi) : ViewModel() {
 
     private val _quote = MutableLiveData<String>()
     val quote: LiveData<String> = _quote
@@ -28,7 +24,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     fun initialize(navigator: MainNavigator) {
         this.navigator = navigator
 
-        quoteApi.getQuoteOfDay()
+        quotesApi.getQuoteOfDay()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeBy(
