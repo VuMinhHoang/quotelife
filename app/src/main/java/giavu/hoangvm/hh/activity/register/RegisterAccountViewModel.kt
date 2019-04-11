89650e8575b6ef09817cdccebd9214a42d201cce
vegeta20 <hoangvmh@gmail.com>
2019-04-11 15:24:59 +0900

    Implement listener by using Livedata instead of navigator

--- app/src/main/java/giavu/hoangvm/hh/activity/register/RegisterAccountViewModel.kt
+++ app/src/main/java/giavu/hoangvm/hh/activity/register/RegisterAccountViewModel.kt
@@ -2,10 +2,12 @@ package giavu.hoangvm.hh.activity.register
 
 import android.annotation.SuppressLint
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import giavu.hoangvm.hh.api.UserApi
 import giavu.hoangvm.hh.helper.ResourceProvider
+import giavu.hoangvm.hh.model.LoginResponse
 import giavu.hoangvm.hh.model.RegBody
 import giavu.hoangvm.hh.model.RegUser
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -18,9 +20,32 @@ import io.reactivex.schedulers.Schedulers
  * @Author: Hoang Vu
  * @Date:   2019/01/14
  */
-class RegisterAccountViewModel(private val resourceProvider: ResourceProvider, private val userApi: UserApi) : ViewModel(){
+class RegisterAccountViewModel(
+    resourceProvider: ResourceProvider,
+    private val userApi: UserApi
+) : ViewModel() {
 
-    private lateinit var navigator: RegisterAccountNavigator
+    sealed class RegisterResult {
+        class Success(val response: LoginResponse) : RegisterResult()
+        class Failure(val throwable: Throwable) : RegisterResult()
+    }
+
+    private val _showProgressRequest: MutableLiveData<Unit> = MutableLiveData()
+    private val _hideProgressRequest: MutableLiveData<Unit> = MutableLiveData()
+    private val _registerResult: MutableLiveData<RegisterResult> = MutableLiveData()
+    private val _gotoLogin: MutableLiveData<Unit> = MutableLiveData()
+
+    val showProgressRequest: LiveData<Unit>
+        get() = _showProgressRequest
+
+    val hideProgressRequest: LiveData<Unit>
+        get() = _hideProgressRequest
+
+    val registerResult: LiveData<RegisterResult>
+        get() = _registerResult
+
+    val gotoLogin: LiveData<Unit>
+        get() = _gotoLogin
 
     val userName = MutableLiveData<String>()
     val email = MutableLiveData<String>()
@@ -33,12 +58,8 @@ class RegisterAccountViewModel(private val resourceProvider: ResourceProvider, p
         _resourceProvider = resourceProvider
     )
 
-    fun initialize(navigator: RegisterAccountNavigator) {
-        this.navigator = navigator
-    }
-
     fun gotoLogin() {
-        navigator.toLogin()
+        _gotoLogin.value = Unit
     }
 
     @SuppressLint("CheckResult")
@@ -53,15 +74,15 @@ class RegisterAccountViewModel(private val resourceProvider: ResourceProvider, p
         )
         userApi.register(user)
             .subscribeOn(Schedulers.io())
-            .doOnSubscribe { navigator.showProgress() }
-            .doFinally { navigator.hideProgress() }
+            .doOnSubscribe { _showProgressRequest.value = Unit }
+            .doFinally { _hideProgressRequest.value = Unit }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeBy(
                 onError = { error ->
-                    navigator.toError(error)
+                    _registerResult.value = RegisterResult.Failure(error)
                 },
                 onSuccess = { response ->
-                    navigator.register(response)
+                    _registerResult.value = RegisterResult.Success(response)
                 }
             )
     }
