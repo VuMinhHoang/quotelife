0061aed703132c7299581c183c71fc7dbd798923
vegeta20 <hoangvmh@gmail.com>
2019-05-15 08:48:39 +0900

    Refact

---
+++ app/src/main/java/giavu/co/jp/activity/daily_quote/DailyQuote.kt
@@ -0,0 +1,13 @@
+package giavu.co.jp.activity.daily_quote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import giavu.co.jp.R
+
+class DailyQuote : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_quote)
+    }
+}
---
+++ app/src/main/java/giavu/co/jp/activity/daily_quote/DailyQuoteViewModel.kt
@@ -0,0 +1,10 @@
+package giavu.co.jp.activity.daily_quote
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @Author: Hoang Vu
+ * @Date:   2019/04/29
+ */
+class DailyQuoteViewModel: ViewModel() {
+}
